17269a3b4f49fa641397aa5306272509ba16ac2d
Danna Bastidas <dannabas2001@gmail.com>
2022-06-09 16:37:23 -0500

    perfil

--- src/Containers/Perfil.jsx
+++ src/Containers/Perfil.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Back, Botones, Button, CadImg, Col2, Cont, Conthead, Desc, Foll, Foto, Header, Imagenes, Palabras, Signal, Textos, Types } from '../styles/Perfil'
 
 const Perfil = () => {
     return (
-        <div><div> <img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654805847/AuraHooks/False_oloyq1.png" alt="" /><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654805881/AuraHooks/Group_14_sriagl.png" alt="" /></div>
-            <section><div><h4>10.7M</h4> <p>Followers</p></div>
-                <div><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806088/AuraHooks/Group_12_czk9tx.png" alt="" />
-                    <h6>Jennie Kim</h6><p>J.Hello Guys <br/> Follow me and like my post </p> <div> <h4>108.3M</h4> <p>Likes</p></div></div>
-                    <button>Follow</button> <button>Messages</button>
-    </section >
-    <section><div><p>Photos</p> <p>Videos</p><p>Album</p><p>Tag</p></div>
-    <div><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806089/AuraHooks/Group_16_cvqgss.png" alt="" />
-    <img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806092/AuraHooks/Group_17_xv6tdn.png" alt="" />
-    </div><div><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806089/AuraHooks/Group_16_cvqgss.png" alt="" /><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806091/AuraHooks/Group_15_inxifq.png" alt="" /></div></section></div >
+        <Cont><Conthead> <Header src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654805847/AuraHooks/False_oloyq1.png" alt="" /><Signal src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654805881/AuraHooks/Group_14_sriagl.png" alt="" /></Conthead>
+            <Textos><Foll><h4>10.7M</h4> <p>Followers</p></Foll>
+                <Foto   ><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806088/AuraHooks/Group_12_czk9tx.png" alt="" />
+                    <h6>Jennie Kim</h6><Desc>J.Hello Guys <br/> Follow me and like my post </Desc></Foto  > <Foll> <h4>108.3M</h4> <p>Likes</p></Foll>
+                    
+    </Textos >
+    <Botones><Button>Follow</Button> <Button>Messages</Button></Botones>
+    <Back><Types><Palabras>Photos</Palabras> <Palabras>Videos</Palabras><Palabras>Album</Palabras><Palabras>Tag</Palabras></Types>
+    <Imagenes><div><CadImg src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806089/AuraHooks/Group_16_cvqgss.png" alt="" />
+    <CadImg src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806092/AuraHooks/Group_17_xv6tdn.png" alt="" />
+    </div><Col2><CadImg src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806089/AuraHooks/Group_16_cvqgss.png" alt="" /><CadImg src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806091/AuraHooks/Group_15_inxifq.png" alt="" /></Col2></Imagenes></Back></Cont>
   )
 }
 
