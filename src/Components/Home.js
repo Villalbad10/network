f5332f59dc82f3e34c6f950b42527aa42489ad60
Jenny Espitia <jennyme971213@gmail.com>
2022-06-09 15:31:40 -0500

    cambios

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div>
+            <img src='https://res.cloudinary.com/dg29vcpk7/image/upload/v1654805779/FindyApp/LOGOLOGO_3_v5ciw4.png' alt='logo'/>
+            <div style={{'display':'flex', 'width':'20%'}} >
+            </div>
+        </div>
+
+
+    </div>
   )
 }
 
