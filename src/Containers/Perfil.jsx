f114325af5cce15e0ebb2dfdd435dac0f60bedf8
Danna Bastidas <dannabas2001@gmail.com>
2022-06-09 15:39:36 -0500

    estructura

---
+++ src/Containers/Perfil.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Perfil = () => {
+    return (
+        <div><div> <img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654805847/AuraHooks/False_oloyq1.png" alt="" /><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654805881/AuraHooks/Group_14_sriagl.png" alt="" /></div>
+            <section><div><h4>10.7M</h4> <p>Followers</p></div>
+                <div><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806088/AuraHooks/Group_12_czk9tx.png" alt="" />
+                    <h6>Jennie Kim</h6><p>J.Hello Guys <br/> Follow me and like my post </p> <div> <h4>108.3M</h4> <p>Likes</p></div></div>
+                    <button>Follow</button> <button>Messages</button>
+    </section >
+    <section><div><p>Photos</p> <p>Videos</p><p>Album</p><p>Tag</p></div>
+    <div><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806089/AuraHooks/Group_16_cvqgss.png" alt="" />
+    <img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806092/AuraHooks/Group_17_xv6tdn.png" alt="" />
+    </div><div><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806089/AuraHooks/Group_16_cvqgss.png" alt="" /><img src="https://res.cloudinary.com/dcsn54xoj/image/upload/v1654806091/AuraHooks/Group_15_inxifq.png" alt="" /></div></section></div >
+  )
+}
+
+export default Perfil
