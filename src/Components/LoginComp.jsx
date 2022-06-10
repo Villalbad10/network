ee5e94f389f5106e02dee150f186a1c6c1ae080e
Danna Bastidas <dannabas2001@gmail.com>
2022-06-09 20:37:45 -0500

    login funcional

--- src/Components/LoginComp.jsx
+++ src/Components/LoginComp.jsx
@@ -1,55 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button, Form } from 'react-bootstrap'
 import getData from '../helpers/getData';
-<<<<<<< HEAD
 import FormHook from '../Hooks/HookForm';
-=======
 import { Btn, FormDiv, FormInput, LoginDiv } from '../styles/styles';
->>>>>>> 7fce9e1d0f0a1a5a6cb666232cb367b7741f23f0
 
 const LoginComp = () => {
+   const navigate = useNavigate()
    const [data, handleChange, reset] = FormHook({
       email:"",
       pass:""
    })
 
    useEffect(() => {
-      getData()
-         .then(res => console.log(res))
-   }, [])
+     console.log(data)
+   }, [data])
 
-   // const handleSubmit = (e) => {
-   //    e.preventDefault();
-   //    const usedata = data.find((user) => user.email === inputs.email)
-   //    console.log(usedata);
-   // }
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      const datos= await getData()
+      const email = data.email;
+      const password = data.pass;
+      datos.forEach(element => {
+         const { email: emailUsuario, pass:contraseña } = element;
+         if (emailUsuario== email){
+            if(contraseña==password){
+             reset()
+             navigate('/home')}
+             else{alert('Contraseña incorrecta')}
+            }})}
+         
 
 
    return (
-<<<<<<< HEAD
-      <>
-         <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-               <Form.Label>Email address</Form.Label>
-               <Form.Control name="email" onChange={handleChange} type="email" placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-               <Form.Label>Password</Form.Label>
-               <Form.Control name="pass" onChange={handleChange} type="password" placeholder="Password" />
-=======
    <LoginDiv>
           <img src='https://res.cloudinary.com/dg29vcpk7/image/upload/v1654821647/FindyApp/LOGOLOGO-2_niodaj.png' alt='logoFrom'/>
            <FormDiv onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail" style={{'display':'flex', 'flexDirection':'column', 'width':'80%','margin':'0 auto', 'gap':'5px', 'marginTop':'20px'}}>
                <Form.Label>E m a i l  A d d r e s s</Form.Label>
-               <FormInput name="email" onChange={handleValue} type="email" placeholder="Enter email" />
+               <FormInput name="email" onChange={handleChange} type="email" placeholder="Enter email" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword" style={{'display':'flex', 'flexDirection':'column', 'width':'80%','margin':'0 auto', 'gap':'5px', 'marginTop':'20px'}}>
                <Form.Label>P a s s w o r d</Form.Label>
-               <FormInput name="pass" onChange={handleValue} type="password" placeholder="Password" />
->>>>>>> 7fce9e1d0f0a1a5a6cb666232cb367b7741f23f0
+               <FormInput name="pass" onChange={handleChange} type="password" placeholder="Password" />
             </Form.Group>
             <Btn variant="outline-primary" type="submit">
                Submit
@@ -57,6 +51,5 @@ const LoginComp = () => {
          </FormDiv>
       </LoginDiv>
    )
-}
-
+   }
 export default LoginComp
